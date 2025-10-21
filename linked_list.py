1b93200725c9deeb38ab197525605049fd6ce789
unknown <dhdbbdhd161@gmail.com>
2025-10-22 07:39:29 +0800

    displays the existing linked list if exists and checks if none

--- linked_list.py
+++ linked_list.py
@@ -12,3 +12,14 @@ class LinkedList:
         new_node = Node(d, self.head)
         self.head = new_node
         self.size += 1
+
+    def display(self):
+        this_node = self.head #checks if current node is the head
+        if not this_node:
+            print("List is empty.") #if empty return none
+            return
+        print("Linked List:", end=" ")
+        while this_node: #checks through the linked list
+            print(this_node.get_data(), end=" -> ")
+            this_node = this_node.get_next()
+        print("None") #prints None at the very end of the linked list after the last node
