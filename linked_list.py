2307244509c4e581e7cd91891f7f894d28e26ffd
unknown <dhdbbdhd161@gmail.com>
2025-10-22 07:20:36 +0800

    return size method and add a new node method

--- linked_list.py
+++ linked_list.py
@@ -4,3 +4,11 @@ class LinkedList:
     def init(self, n = None):
         self.head = n
         self.size = 0
+
+    def get_list_size(self): #returns the size of the linked list
+        return self.size
+
+    def add_new_node(self, d): #adds a new node in linked list
+        new_node = Node(d, self.head)
+        self.head = new_node
+        self.size += 1
