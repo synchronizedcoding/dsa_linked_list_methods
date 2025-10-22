0cb3c5693ecb68e1ac8d0906b96814ba380b9b6a
unknown <dhdbbdhd161@gmail.com>
2025-10-22 09:02:08 +0800

    main-menu, gets the user's choice and calls the method to use

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from node import Node
 from linked_list import LinkedList
 
+myList = LinkedList()
+print("\nWelcome to Linked-List Methods Program\n")
+while True:
+    print("\n1. Display linked list\n2. Add a new data\n3. Exit")
+
+    user_input = input("\nEnter your choice (1-5): ")
+
+    if user_input == '1':
+        myList.display()
+
+    elif user_input == '2':
+        data = input("Enter a new data: ")
+        myList.add_new_node(data)
+        print(f"Added '{data}' in the linked-list.")
+    else:
+        break
